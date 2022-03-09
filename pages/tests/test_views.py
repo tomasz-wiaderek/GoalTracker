20b37aae6ad2c2df453c65b1e3fd9b862c162692
t.wiaderek <t.wiaderek@gmail.com>
2022-03-09 13:43:19 +0100

    home view tests added

---
+++ pages/tests/test_views.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+
+from pages import views
+
+
+class HomeViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get(reverse('pages:home'))
+
+    def test_if_given_url_runs_correct_view(self):
+        self.assertEqual(self.response.resolver_match.func, views.home)
+
+    def test_if_response_status_is_200(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_if_view_uses_correct_template(self):
+        self.assertTemplateUsed(self.response, 'pages/home.html')
