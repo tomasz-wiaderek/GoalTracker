a6538fab3f84805007be52f548fa4e63dab43631
t.wiaderek <t.wiaderek@gmail.com>
2022-03-10 10:51:46 +0100

    register_user view tests added

---
+++ goal_tracker/tests/test_views.py
@@ -0,0 +1,50 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from goal_tracker.views import register_user
+
+
+class RegisterUserTest(TestCase):
+
+    def setUp(self):
+
+        self.data = {
+            'username': 'TestUser',
+            'email': 'testuser@mail.com',
+            'password1': 'testing321',
+            'password2': 'testing321'
+        }
+
+        self.client = Client()
+
+    def test_register_user_GET_view(self):
+
+        response = self.client.get(reverse('register'))
+
+        self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
+        self.assertEqual(response.resolver_match.func, register_user)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_register_user_POST_view(self):
+
+        response = self.client.post(reverse('register'), data=self.data)
+
+        self.assertEqual(response.request['REQUEST_METHOD'], 'POST')
+        self.assertEqual(response.resolver_match.func, register_user)
+        self.assertEqual(response.status_code, 302)
+
+    def test_if_register_user_POST_view_creates_new_user(self):
+
+        self.client.post(reverse('register'), data=self.data)
+        created_user = User.objects.get(username='TestUser')
+
+        self.assertEqual(created_user.username, self.data['username'])
+        self.assertEqual(created_user.email, self.data['email'])
+
+    def test_if_register_user_POST_view_redirects_correctly(self):
+
+        response = self.client.post(reverse('register'), data=self.data)
+
+        self.assertRedirects(response, '/login/')
