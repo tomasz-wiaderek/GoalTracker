a17d2f8add135137f852a60d74a4b9b0c77bc1b1
t.wiaderek <t.wiaderek@gmail.com>
2024-01-03 18:53:17 +0100

    urls fixed

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 app_name = 'pages'
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
 ]
